63383ba1810a52f3da96351410cb652c8ba26ad3
unknown <pedroericcarneiro@alu.ufc.br>
2023-10-08 23:34:08 -0300

    Version 0

---
+++ src/Aluno.java
@@ -0,0 +1,9 @@
+
+public class Aluno extends Compradores{
+
+	public Aluno(String nome, int matricula) {
+		super(nome, matricula);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/Servidor.java
@@ -0,0 +1,9 @@
+
+public class Servidor extends Compradores{
+
+	public Servidor(String nome, int matricula) {
+		super(nome, matricula);
+		// TODO Auto-generated constructor stub
+	}
+
+}
